2c7bf8a7b3a1c340103f3c9ed87e59b1bab76649
sanishajain <ishasan82@gmail.com>
2026-01-14 22:19:29 +0530

    Fix cookie auth + CORS for Render

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,5 +4,14 @@ const { register, login } = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", (req, res) => {
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+    });
+    res.json({ message: "Logged out" });
+});
+
 
 module.exports = router;
