a5cd2bfa523b1e43ba048926994234b321c57490
sanishajain <ishasan82@gmail.com>
2026-01-14 14:56:33 +0530

    Fix CORS and credentials for production

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,6 @@ app.get("/", (req, res) => {
   res.send("GigFlow API running");
 });
 
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on " + PORT));
